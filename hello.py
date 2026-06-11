863e3d500039be3a242f40597ca8e638f09b9742
Marlon Porta <mpm-project@outlook.com>
2026-06-10 18:32:13 -0600

    Week 1 complete: hello script with generation checker

--- hello.py
+++ hello.py
@@ -1,15 +1,31 @@
 #Week 1 - Hello World script 
-#Goal: practice inpout, variable, and basic math
+#Goal: practice input, variable, and basic math
 
 
-name = input("What is yuour name? ")
+name = input("What is your name? ")
 birth_year = int(input("What year were you born? "))
 
 current_year = 2026
 age = current_year - birth_year
-years_ahed = 60 - age
+years_ahead = 60 - age
 
 print("")
 print(f"Welcome, {name}!")
-print(f"you are {age} years old")
-print(f"You have {years_ahed} years of coding ahed of you.")
+print(f"You are {age} years old")
+print(f"You have {years_ahead} years of coding ahead of you.")
+
+if birth_year >= 1946 and birth_year <= 1964:
+    print("")
+    print("You are a Baby Boomer!")
+elif birth_year >= 1965 and birth_year <= 1980:
+    print("")
+    print("You are a Gen X!")
+elif birth_year >= 1981 and birth_year <= 1996:
+    print("")
+    print("You are a Millenial!")
+elif birth_year >= 1997 and birth_year <= 2012: 
+    print("")
+    print("You are a Gen Z!")
+else:
+    print("")
+    print("You are too old or too young for our list!")
