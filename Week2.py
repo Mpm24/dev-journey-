030ebc2fa8ba94a31813da52b24d22b948d470b9
Marlon Porta <mpm-project@outlook.com>
2026-06-17 18:24:24 -0600

    Week 2 complete grade tracker with lists and loops

---
+++ Week2.py
@@ -0,0 +1,18 @@
+# Week 2 - Grade Tracker
+# Goal: practice lists and loops
+
+
+number_of_grades = int(input("Enter the number of grades: "))
+
+grades = [] 
+
+for i in range(number_of_grades):
+    grade = int(input(f"Enter grade {i+1}; "))
+    grades.append(grade)
+
+
+print("")
+print(f"Your grades: {grades}")
+print(f"Highest grade: {max(grades)}")
+print(f"Lowest grade: {min(grades)}")
+print(f"Average grade: {sum(grades) / len(grades):.2f}")
