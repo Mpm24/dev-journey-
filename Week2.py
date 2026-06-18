de881ced0163500c792c8e0565ad6f2f9d43900a
Marlon Porta <mpm-project@outlook.com>
2026-06-17 18:36:19 -0600

    Week 2 complete: grade tracker with pass/fail

--- Week2.py
+++ Week2.py
@@ -7,12 +7,19 @@ number_of_grades = int(input("Enter the number of grades: "))
 grades = [] 
 
 for i in range(number_of_grades):
-    grade = int(input(f"Enter grade {i+1}; "))
+    grade = int(input(f"Enter grade {i+1}: "))
     grades.append(grade)
 
+average = sum(grades) / len(grades)
 
 print("")
 print(f"Your grades: {grades}")
 print(f"Highest grade: {max(grades)}")
 print(f"Lowest grade: {min(grades)}")
-print(f"Average grade: {sum(grades) / len(grades):.2f}")
+print(f"Average grade: {average:.2f}")
+
+
+if average >= 70:
+    print("Result: PASS ✅")
+else:
+    print("Result: FAIL ❌")
